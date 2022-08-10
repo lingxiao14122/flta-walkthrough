ca75a92d4fa2e568257c1e8dc26c8114932ca661
ling xiao <lingxiao3218@gmail.com>
2022-08-11 01:21:48 +0800

    remove test folders

--- chapter2/rec/android/app/src/main/kotlin/com/example/rec/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.rec
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- chapter2/reci/android/app/src/main/kotlin/com/example/reci/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.reci
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
