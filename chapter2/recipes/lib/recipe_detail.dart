26316a91f90c05f19856028aca1f87b9eddcbbc0
ling xiao <lingxiao3218@gmail.com>
2022-08-11 18:56:08 +0800

    chapter 2 end

--- chapter2/recipes/lib/recipe_detail.dart
+++ chapter2/recipes/lib/recipe_detail.dart
@@ -16,7 +16,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
   @override
   Widget build(BuildContext context) {
     // 1
@@ -46,8 +46,38 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
-            // TODO: Add Expanded
-            // TODO: Add Slider() here
+            Expanded(
+              // 8
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // 9
+                  return Text('${ingredient.quantity * _sliderVal} '
+                      '${ingredient.measure} ${ingredient.name}');
+                  },
+              ),
+            ),
+            Slider(
+              // 10
+              min: 1,
+              max: 10,
+              divisions: 10,
+              // 11
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              // 12
+              value: _sliderVal.toDouble(),
+              // 13
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              // 14
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
